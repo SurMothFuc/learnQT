a224b41ffd9338fb8ea07da9371dce759c34e458
xiong <517018432@qq.com>
2026-02-08 15:07:33 +0800

    update renderparam

--- include/RenderParams.h
+++ include/RenderParams.h
@@ -4,36 +4,41 @@
 
 #include <atomic>
 
+#define RENDER_PARAMS_PARAM(Camel, getter, type, member, defaultValue) \
+public: \
+    type getter() const { \
+        m_reads.fetch_add(1, std::memory_order_relaxed); \
+        return member.load(std::memory_order_relaxed); \
+    } \
+    void set##Camel(type v) { \
+        const type old = member.exchange(v, std::memory_order_relaxed); \
+        m_writes.fetch_add(1, std::memory_order_relaxed); \
+        if (old != v) { \
+            emit getter##Changed(v); \
+        } \
+    } \
+    Q_SIGNAL void getter##Changed(type value); \
+private: \
+    std::atomic<type> member{defaultValue};
+
 class RenderParams : public QObject {
     Q_OBJECT
 
 public:
     static RenderParams& instance();
 
-#define RENDER_PARAMS_PARAM_LIST(X) \
-    X(Denoise, denoise, bool, m_denoise, true) \
-    X(RenderLow, renderLow, bool, m_renderLow, false) \
-    X(UseTileRendering, useTileRendering, bool, m_useTileRendering, true) \
-    X(UseEnvironmentMap, useEnvironmentMap, bool, m_useEnvironmentMap, true) \
-    X(TileSize, tileSize, int, m_tileSize, 240)
+    RENDER_PARAMS_PARAM(Denoise, denoise, bool, m_denoise, true)
+    RENDER_PARAMS_PARAM(RenderLow, renderLow, bool, m_renderLow, false)
+    RENDER_PARAMS_PARAM(UseTileRendering, useTileRendering, bool, m_useTileRendering, true)
+    RENDER_PARAMS_PARAM(UseEnvironmentMap, useEnvironmentMap, bool, m_useEnvironmentMap, true)
+    RENDER_PARAMS_PARAM(TileSize, tileSize, int, m_tileSize, 240)
 
-#define DECLARE_PARAM_API(Camel, getter, type, member, defaultValue) \
-    type getter() const; \
-    void set##Camel(type v);
-    RENDER_PARAMS_PARAM_LIST(DECLARE_PARAM_API)
-#undef DECLARE_PARAM_API
-
+public:
     struct Stats {
         unsigned long long reads;
         unsigned long long writes;
     };
     Stats stats() const;
-
-signals:
-#define DECLARE_CHANGED_SIGNAL(Camel, getter, type, member, defaultValue) \
-    void getter##Changed(type value);
-    RENDER_PARAMS_PARAM_LIST(DECLARE_CHANGED_SIGNAL)
-#undef DECLARE_CHANGED_SIGNAL
 
 private:
     explicit RenderParams(QObject* parent = nullptr);
@@ -42,12 +47,8 @@ private:
     RenderParams(RenderParams&&) = delete;
     RenderParams& operator=(RenderParams&&) = delete;
 
-#define DECLARE_PARAM_STORAGE(Camel, getter, type, member, defaultValue) \
-    std::atomic<type> member{defaultValue};
-    RENDER_PARAMS_PARAM_LIST(DECLARE_PARAM_STORAGE)
-#undef DECLARE_PARAM_STORAGE
-#undef RENDER_PARAMS_PARAM_LIST
-
     mutable std::atomic<unsigned long long> m_reads{0};
     mutable std::atomic<unsigned long long> m_writes{0};
 };
+
+#undef RENDER_PARAMS_PARAM
--- src/RenderParams.cpp
+++ src/RenderParams.cpp
@@ -9,29 +9,6 @@ RenderParams::RenderParams(QObject* parent)
     : QObject(parent) {
 }
 
-#define RENDER_PARAMS_PARAM_LIST(X) \
-    X(Denoise, denoise, bool, m_denoise, true) \
-    X(RenderLow, renderLow, bool, m_renderLow, false) \
-    X(UseTileRendering, useTileRendering, bool, m_useTileRendering, true) \
-    X(UseEnvironmentMap, useEnvironmentMap, bool, m_useEnvironmentMap, true) \
-    X(TileSize, tileSize, int, m_tileSize, 240)
-
-#define DEFINE_PARAM_API(Camel, getter, type, member, defaultValue) \
-    type RenderParams::getter() const { \
-        m_reads.fetch_add(1, std::memory_order_relaxed); \
-        return member.load(std::memory_order_relaxed); \
-    } \
-    void RenderParams::set##Camel(type v) { \
-        type old = member.exchange(v, std::memory_order_relaxed); \
-        m_writes.fetch_add(1, std::memory_order_relaxed); \
-        if (old != v) { \
-            emit getter##Changed(v); \
-        } \
-    }
-RENDER_PARAMS_PARAM_LIST(DEFINE_PARAM_API)
-#undef DEFINE_PARAM_API
-#undef RENDER_PARAMS_PARAM_LIST
-
 RenderParams::Stats RenderParams::stats() const {
     return {m_reads.load(), m_writes.load()};
 }
